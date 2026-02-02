744230f4e0f5b94c1d5f45377508697f6d0a1f8a
Daniel <dsanjuan1999@gmail.com>
2026-02-02 18:25:32 -0300

    Primer Commit

---
+++ src/components/TestItem.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="item">
+    <i>
+      <slot name="coso"></slot>
+    </i>
+    <div class="detallito">
+      <h5>
+        <slot name="titulito"></slot>
+      </h5>
+      <p style="text-align: justify; font-size: 25px; color: #00cc00">
+        <slot></slot>
+      </p>
+    </div>
+  </div>
+</template>
